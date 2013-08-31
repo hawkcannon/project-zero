202060216e3e4456985192532cfce393acfc0b5f
hawkcannon <hawkcannon@gmail.com>
2013-08-31 18:07:12 -0500

    Edited the pom.xml so LWJGL works, then added a bare-bones program for the MainGame.

--- src/main/java/com/catcannon/projectzero/main/MainGame.java
+++ src/main/java/com/catcannon/projectzero/main/MainGame.java
@@ -1,9 +1,46 @@
 package com.catcannon.projectzero.main;
 
+import org.lwjgl.LWJGLException;
+import org.lwjgl.opengl.Display;
+import org.lwjgl.opengl.DisplayMode;
+
 /**
  * Placeholder, for now.
  *
  */
 public class MainGame {
-
+	public static void main(String[] args){
+		MainGame game = new MainGame();
+		
+		try {
+			game.start();
+			
+			while(!Display.isCloseRequested()){
+				game.update();
+			}
+			
+			game.stop();
+		} catch (LWJGLException e) {
+			e.printStackTrace();
+		}
+		
+	}
+	
+	public void start() throws LWJGLException{
+		Display.setDisplayMode(new DisplayMode(800, 600));
+		Display.create();
+		
+		while(!Display.isCloseRequested()){
+			Display.update();
+		}		
+	}
+	
+	public void update(){
+		
+		Display.update();
+	}
+	
+	public void stop(){
+		Display.destroy();
+	}
 }
